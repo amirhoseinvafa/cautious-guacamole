39337c876a9bfea32904a03316798f96c1281907
vafa <amirhosein.v1994@gmail.com>
2025-08-27 02:09:53 +0330

    Add factorial functions and implement 0/1 and fractional knapsack algorithms

--- fact.js
+++ fact.js
@@ -5,5 +5,16 @@ function fact(number) {
   }
   return result;
 }
+// T = O(n)
+
+//  ------------------------------------
+
+function fact1(number) {
+  if (number === 0) return 1;
+
+  return number * fact1(number - 1);
+}
+// T = O(n)
 
 console.log(fact(4));
+console.log(fact1(4));
